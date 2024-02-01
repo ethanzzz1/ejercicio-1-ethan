0cd64bc3384ed5b692dde15f24f43f17efb72977
Ethan <20230006@ricaldone.edu.sv>
2024-02-01 11:18:58 -0600

    Se agrego contenido al css

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert() {
+    var text1 = document.getElementById("Nombre").value;
+    var text2 = document.getElementById("Apellido").value;
+  
+    alert("Nombre: " + text1 + "\nApellido: " + text2);
+  }
